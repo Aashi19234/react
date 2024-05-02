690ace28a59b888beac0edb7b0dbc615b659fa57
Aashi19234 <aasthaj375@gmail.com>
2024-05-02 17:21:05 +0530

    lecture 4 completed

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,16 @@
 import Foo from "./new"
 
 function App() {
-  
+  // how to inject js or variables in jsx
+  // x-> it refers to the html part
+  const username="chai aur code"
+  // {username}-> this is called evaluated expression bcoz here we are not writing entire js just its outcome
+   // here we can only write final outcome
+   // all the evaluation part has to be done before this, here in the function part not in return statement
+   //loops, condition cannot be written in return statement because 
   return (
     <>
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1>
     <Foo/>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,17 +3,52 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 // we can create a function here itself and call it
- function MyApp(){
+ /*function MyApp(){
   return (
     <div>
       <h1>Custom App</h1>
     </div>
   )
- }
+ }*/
+ /*const Reactelement ={
+  type:'a',
+  props:{
+      href:'https://google.com',
+      target:'_blank'
+
+  },
+  children:'Click me to visit google'
+}*/
+
+const anotherelement=(
+  <a href="httpe://google.com" target='_blank'> Visit Google</a>
+)
+
+const anotheruser="chai aur react"
+
+// element creation from React as per the import
+const reactelement=React.createElement(
+  // enter a tag as first parameter
+  'a',
+  // second parameter exepected is object which is same as setattribute
+  {href: 'https://google.com', target:'_blank'},
+  // direct text as third parameter
+  'click me to visit google',
+  // after entire tree is made then we can inject variables
+anotheruser
+
+
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MyApp />
-  </React.StrictMode>,
+  
+   reactelement
+  
 )
+
+//  if we call reactelement here in reactdom render then it will not work because the parameters of this object is not as per the render which
+// react created , it is as per the custom render which we created thats why it will not work
+
+// anotherelement will work because it contains a normal a tag
 
