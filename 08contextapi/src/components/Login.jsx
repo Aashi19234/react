344fe7305c9986fdb150e5a43aedc084ceac3b2c
Aashi19234 <aasthaj375@gmail.com>
2024-05-17 00:29:48 +0530

    project 8 done

--- 08contextapi/src/components/Login.jsx
+++ 08contextapi/src/components/Login.jsx
@@ -1,28 +1,47 @@
 import React, {useState,useContext} from 'react'
 import UserContext from '../context/UserContext'
-import 
-function Login() {
-    const [username,setusername] =useState('')
-    const [password,setpassword] =useState('')
 
-    const {setUser}=useContext(UserContext)// we have taken setuser pehle in usercontextprovider
+function Login() {
+    const [username,setUsername] =useState('')
+    const [password,setPassword] =useState('')
+
+    
+    const {setUser}=useContext(UserContext)
+    // we have taken setuser pehle in usercontextprovider
+// here we are consuming context values through useContext hook
+//Any component that is nested within UserContextProvider can access the context values using useContext.
 
     const handlesubmit=(e)=>{
         e.preventDefault()
         setUser({username,password})
 
-    }
+    }// ise new values bhejdiaa
+    /*
+    setUser is a function: It comes from something called UserContext, which is a way to share data across different parts of your app without passing it through props.
+
+What setUser does: When you call setUser and give it an object with username and password, it updates the user information stored in UserContext.
+
+Why this is useful: By updating the user information in UserContext, any part of your app that is connected to UserContext will automatically get the updated user information. This means you don't have to manually pass the new user data to every component that needs it.
+    
+    
+    */
+
+// data lena kese hai i.e data extract kese kre abhi toh user m values set ho gai vaha se len bhi toh pdegi
+
+
     return (
         <div>
         <h2>Login</h2>
         <input type='text'
         value={username}
-        onChange={(e)=>setusername(e.target.value)}
+        onChange={(e)=>setUsername(e.target.value)}
 
         placeholder='username'/>
+        {" "}
+        
         <input type='text'
         value={password}
-        onChange={(e)=>setpassword(e.target.value)}
+        onChange={(e)=>setPassword(e.target.value)}
         placeholder='password'/>
         <button onClick={handlesubmit}>Submit</button>
 
--- 08contextapi/src/context/UserContextProvider.jsx
+++ 08contextapi/src/context/UserContextProvider.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import UserContext from './UserContext'
 
 // file ka naame jsx rkha kyuki hm jo fragments likhenge toh usko top level pr rkhna pdega taki baki sb ka access mile
 
 // step 3
 const UserContextProvider = ({children})=>{
-    const {user,setuser}=React.useState(null)
+    const [user,setUser]=useState(null)
     return (
-        <UserContext.Provider val={{user,setuser}}>
+        <UserContext.Provider value={{user,setUser}}>
         {children}
+         
         </UserContext.Provider>
+
+        /*
+        The UserContext.Provider is a context
+         provider component that makes the user state and setuser function
+          available to any nested components that consume this context.
+        */
+
+          /*
+           This line inserts the children prop, which means
+            any nested components passed into UserContextProvider will be rendered here. 
+           */
     )
 
 }
 
 
 export default UserContextProvider
+
+/*
+Three steps of context
+1) creating a context that we have done in usercontext.js
+2) providing the context values that we have done through usercontextprovider.jsx
+3) consuming these values wherever required which we have done in our components folder
+
+*/
