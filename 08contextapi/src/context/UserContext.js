2d1788d7ab9fd9ac99eb5a4c3ea1874012cf450e
Aashi19234 <aasthaj375@gmail.com>
2024-05-19 00:58:27 +0530

    Project 10

--- 08contextapi/src/context/UserContext.js
+++ 08contextapi/src/context/UserContext.js
@@ -14,7 +14,7 @@ export default UserContext;
 
 
 // Step 2-> context bnane k baad uske saath provider bnana hga
-//provider data provide hga
+//provider data provide krega
 // provider k andr hm required components bnaenge fir usko usercontext m wrap krdenge
 // ab y saare components usercontext k through sari states ka access le skte h
 
